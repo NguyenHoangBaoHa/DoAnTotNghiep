2357bfdc5e029f93ba88ca11ba2d4dc66395dc99
NguyenHoangBaoHa <nguyenhoangbaoha4997@gmail.com>
2024-11-25 20:15:54 +0700

    Delete QuanLySanBong directory

--- QuanLySanBong/Entities/Account/Dto/AccountDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuanLySanBong.Entities.Account.Dto
-{
-    public class AccountDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-    }
-}
