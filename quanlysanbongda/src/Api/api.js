77b5ec1c61b9eb5a2059f52c183bb3b443cfc48f
DESKTOP-IJLO8PN\Admin <nguyenhoangbaoha4997@gmail.com>
2024-12-05 19:27:55 +0700

    Update

--- quanlysanbongda/src/Api/api.js
+++ quanlysanbongda/src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://192.168.0.2:8001/api', // Đảm bảo URL đúng
+  baseURL: 'https://localhost:8001/api', // Đảm bảo URL đúng
   headers: {
     'Content-Type': 'application/json',
   },
