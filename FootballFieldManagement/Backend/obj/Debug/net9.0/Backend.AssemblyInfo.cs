60b904d72f2bca23df54c5b4f5a6371b08722fe0
DESKTOP-IJLO8PN\Admin <nguyenhoangbaoha4997@gmail.com>
2025-02-24 21:59:27 +0700

    update

--- FootballFieldManagement/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ FootballFieldManagement/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74823d43f11924675ecb3a1ecfd40c6b68b046fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb382cb66dbb40daa389b81bf8e3c58cdc54824")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
